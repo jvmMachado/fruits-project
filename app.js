7fd6c1096bc16024bddc99fa418b6dfcf1a47d2c
jvmMachado <joaovmmachado@gmail.com>
2020-02-16 13:40:41 -0300

    Add insertMany function to insert documents into mongodb

--- app.js
+++ app.js
@@ -1,21 +1,53 @@
-const Mongoclient = require('mongodb').MongoClient;
+const MongoClient = require('mongodb').MongoClient;
 const assert = require('assert');
 
 //Connection URL
 const url = 'mongodb://localhost:27017';
 
 //Database Name
-const dbName = 'myproject';
+const dbName = 'fruitsDB';
 
 //create a new MongoClient
-const client = new MongoClient(url);
+const client = new MongoClient(url, {
+    useUnifiedTopology: true
+});
 
 //Use connect method to connect to the Server
-client.connect(function(err) {
+client.connect(function (err) {
     assert.equal(null, err);
-    console.lot('Connected successfully to server');
+    console.log('Connected successfully to server');
 
     const db = client.db(dbName);
 
-    client.close();
-});
+    insertDocuments(db, function() {
+      client.close();
+    });
+  });
+
+const insertDocuments = function (db, callback) {
+    // Get the documents collection
+    const collection = db.collection('fruits');
+    // Insert some documents
+    collection.insertMany([{
+            name: 'Apple',
+            score: 8,
+            review: 'Great fruit'
+        },
+        {
+            name: 'Orange',
+            score: 6,
+            review: 'Kinda sour'
+        },
+        {
+            name: 'Banana',
+            score: 9,
+            review: `Great stuff! It actually comes with it's own wrapper`
+        }
+    ], function (err, result) {
+        assert.equal(err, null);
+        assert.equal(3, result.result.n);
+        assert.equal(3, result.ops.length);
+        console.log("Inserted 3 documents into the collection");
+        callback(result);
+    });
+}
