62a86dc88137db33a41b659dafb4a310fe81de8c
jvmMachado <joaovmmachado@gmail.com>
2020-02-16 13:48:23 -0300

    Add findDocuments function to find all documents in mongodb

--- app.js
+++ app.js
@@ -20,7 +20,9 @@ client.connect(function (err) {
     const db = client.db(dbName);
 
     insertDocuments(db, function() {
-      client.close();
+        findDocuments(db, function() {
+            client.close();
+         });
     });
   });
 
@@ -51,3 +53,15 @@ const insertDocuments = function (db, callback) {
         callback(result);
     });
 }
+
+const findDocuments = function(db, callback) {
+    // Get the documents collection
+    const collection = db.collection('fruits');
+    // Find some documents
+    collection.find({}).toArray(function(err, fruits) {
+      assert.equal(err, null);
+      console.log("Found the following records");
+      console.log(fruits)
+      callback(fruits);
+    });
+  }
